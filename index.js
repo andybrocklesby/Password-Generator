73a85f1ddb65592e713bd493606644de525be065
AndyWilliam96 <153957322+andybrocklesby@users.noreply.github.com>
2024-02-04 18:51:21 -0600

    Update 4

--- index.js
+++ index.js
@@ -1,44 +1,109 @@
-// Array of character, alphabetical, numerical and symbolic
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordDisplayOne = document.getElementById('password-one');
-let passwordDisplayTwo = document.getElementById('password-two');
-let newPasswordOne = "";
-let newPasswordTwo = "";
-let symbolCheckbox = document.getElementById('symbol');
-let numberCheckbox = document.getElementById('number');
+let isDarkMode = false;
+let userInput = document.getElementById('length-input');
 
-// Function to create a random character
-function getRandomCharacter() {
-    // Selecting a random number the size of the array
+// Stretch Goal:
+// [x] Ability to set password length
+// [x] Add copy on click
+// [] Toggle symbols/numbers on/off
+// [x] Dark/light mode
+
+function getRandomCharacter(){
+    const validCharacters = includeNumbers && includeSymbols ? characters : includeNumbers ? characters.slice(0, characters.length - 28) : includeSymbols ? characters.slice(characters.length - 28) : [];
     let randomIndex = Math.floor(Math.random() * characters.length);
-    // Selecting a random character based on the random index
-    let randomCharacter = characters[randomIndex];
-    // Return the character to be used elsewhere
-    return randomCharacter;
+    return characters[randomIndex];
 }
 
 function getPassword(){
-    // Reset password to nothing when function is called
-    let newPasswordOne = "";
-    let newPasswordTwo = "";
-    // Loop through characters array to count
+    let passwordDisplayOne = document.getElementById('display-one');
+    let passwordDisplayTwo = document.getElementById('display-two');
+    passwordDisplayOne.textContent = "";
+    passwordDisplayTwo.textContent = "";
     for(let i = 0; i < characters.length; i++){
-        // If count is less than 15, execute instructions
-        if(i < 15){
-            // Add random character to string
-            newPasswordOne += getRandomCharacter();
-            newPasswordTwo += getRandomCharacter();
+        if(i < userInput.value){
+            if(userInput.value > 20 || userInput.value < 10){
+                produceError();
+            }
+            else{
+                passwordDisplayOne.textContent += getRandomCharacter();
+                passwordDisplayTwo.textContent += getRandomCharacter();
+            }
         }
     }
-    // Render the passwords to the elements
-    passwordDisplayOne.textContent = newPasswordOne;
-    passwordDisplayTwo.textContent = newPasswordTwo;
 }
 
+function toggleAppearance(){
+    let bodyBackground = document.getElementById('body');
+    let appBackground = document.getElementById('app-bg');
+    let toggleButton = document.getElementById('toggle-btn');
+    let title = document.getElementById('title');
+    let titleSpan = document.getElementById('title-span');
+    let subtitle = document.getElementById('subtitle');
+    let inputLabel = document.getElementById('input-label');
+    let generateButton = document.getElementById('generate-btn'); 
+    
+    if(isDarkMode === true){
+        bodyBackground.style.background = "#e9edc9";
+        appBackground.style.background = "#fefae0";
+        toggleButton.style.background = "#e9edc9";
+        toggleButton.textContent = "🌙";
+        title.style.color = "#606c38";
+        titleSpan.style.color = "#d4a373";
+        subtitle.style.color = "#606c38";
+        inputLabel.style.color = "#606c38";
+        generateButton.style.background = "#d4a373";
+        generateButton.style.color = "white";
+        isDarkMode = false;
+    }
+    else if(isDarkMode === false){
+        bodyBackground.style.background = "#344e41";
+        appBackground.style.background = "#3a5a40";
+        toggleButton.style.background = "#344e41";
+        toggleButton.textContent = "☀️";
+        title.style.color = "#dad7cd";
+        titleSpan.style.color = "#a3b18a";
+        subtitle.style.color = "#a3b18a";
+        inputLabel.style.color = "white";
+        generateButton.style.background = "#dad7cd";
+        generateButton.style.color = "#344e41";
+        isDarkMode = true;
+    }
+}
 
+function produceError(){
+    let message = "";
+    let errorEl = document.getElementById("error-display");
+    
+    if(userInput.value > 20){
+        message = "Input is too large";
+        errorEl.textContent = message;
+        setTimeout(function () {
+        errorEl.textContent = "";
+    }, 3000);
+    }
+    else if(userInput.value < 10){
+        message = "Input is too small";
+        errorEl.textContent = message;
+        setTimeout(function () {
+        errorEl.textContent = "";
+    }, 3000);
+    }
+}
 
+function copyTextContent(event) {
+    const textToCopy = event.target.textContent;
+    const textarea = document.createElement('textarea');
+    textarea.value = textToCopy;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alert('Text copied to clipboard: ' + textToCopy);
+}
 
- 
-
+const clickableElements = document.querySelectorAll('.clickable');
+    clickableElements.forEach(element => {
+    element.addEventListener('click', copyTextContent);
+    });
